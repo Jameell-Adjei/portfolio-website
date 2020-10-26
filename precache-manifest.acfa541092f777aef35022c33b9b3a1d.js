7b27c6340463f93b48e1de6621f6c9bcab3b40c4
JayAlmighty00 <jameelladjei@gmail.com>
2020-10-26 18:31:37 -0400

    Updates

---
+++ precache-manifest.acfa541092f777aef35022c33b9b3a1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71fc357763340239ba56598a9343ffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bed8d863994dbbed25e",
+    "url": "/static/css/main.c24984e6.chunk.css"
+  },
+  {
+    "revision": "5e7e1f92c495fefe1b0e",
+    "url": "/static/js/2.0cf34fe8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0cf34fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bed8d863994dbbed25e",
+    "url": "/static/js/main.fed89e4a.chunk.js"
+  },
+  {
+    "revision": "35ec47dfee4c65c275be",
+    "url": "/static/js/runtime-main.c6e37e88.js"
+  },
+  {
+    "revision": "119d6830ad2e11a1c5cdc0a411cab3d9",
+    "url": "/static/media/Resume.119d6830.pdf"
+  }
+]);
